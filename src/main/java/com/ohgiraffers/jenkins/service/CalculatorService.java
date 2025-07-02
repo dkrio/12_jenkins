9798fe8eb9564a652751d9bb4153f26439502b47
dkrio <dkrio34@gmail.com>
2025-07-02 09:34:48 +0900

    jenkins 연동 테스트

--- src/main/java/com/ohgiraffers/jenkins/service/CalculatorService.java
+++ src/main/java/com/ohgiraffers/jenkins/service/CalculatorService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
-
+    // push용 주석
     public int plusTwoNumbers(CalculatorDto calculatorDto) {
         return calculatorDto.getNum1() + calculatorDto.getNum2();
     }
